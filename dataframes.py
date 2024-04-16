5bd1d63f66667abc85af18fedd75167613c33d4f
Luis Fernando Saboia <135769669+luisflsj@users.noreply.github.com>
2024-04-16 20:16:49 -0300

    Update dataframes.py
    modificação de endereço

--- dataframes.py
+++ dataframes.py
@@ -1,3 +1,3 @@
 import pandas as pd
 
-df_financeiro = pd.read_csv("dados\movimentacoes_financeiras.csv", encoding='latin-1', delimiter=";")
+df_financeiro = pd.read_csv("movimentacoes_financeiras.csv", encoding='latin-1', delimiter=";")
