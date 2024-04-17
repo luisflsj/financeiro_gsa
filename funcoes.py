739c29eae7494fa013ad63f71a00b3c9258731be
Luis Fernando Saboia <135769669+luisflsj@users.noreply.github.com>
2024-04-17 13:05:07 -0300

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import pandas as pd
-from fpdf import FPDF
 from datetime import datetime
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_pdf import PdfPages
+from fpdf import FPDF
 # ======================= FORMATAÇÃO FINANCEIRA ======================= #
 
 def format_number(value, prefix=''):
@@ -31,3 +29,36 @@ def converter_data(data_str):
         return datetime.strptime(data_str, '%d/%m/%Y').date()
     else:
         return data_str
+    
+# ======================= DOWNLOAD DE DATAFRAMES ======================= #
+
+def generate_pdf(df):
+    pdf = FPDF(orientation='L')  # Definir orientação como paisagem
+    pdf.add_page()
+
+    # Definir título da página
+    pdf.set_font("Arial", size=7)
+    pdf.cell(280, 10, txt="Tabela Financeira", ln=True, align="R")  # Ajustar largura para modo paisagem
+
+    # Adicionar dados da tabela
+    col_widths = [40] * len(df.columns)  # Inicializar larguras das colunas
+    row_height = 10
+
+    # Calcular larguras ideais das colunas com base no tamanho dos dados
+    for i, col in enumerate(df.columns):
+        max_width = pdf.get_string_width(col)  # Largura máxima da coluna
+        for value in df[col]:
+            value_width = pdf.get_string_width(str(value))
+            max_width = max(max_width, value_width)
+        col_widths[i] = max_width + 6  # Adicionar algum espaço extra
+
+    # Adicionar dados da tabela com larguras ajustadas
+    for row in df.values:
+        for i, item in enumerate(row):
+            pdf.cell(col_widths[i], row_height, txt=str(item), border=1)
+        pdf.ln(row_height)
+
+    pdf_filename = "dataframe.pdf"
+    pdf.output(pdf_filename)
+    return pdf_filename
+
